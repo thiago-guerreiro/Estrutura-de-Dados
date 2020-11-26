38b969f33728b23dd78ef4918a6919fcde0855a2
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2020-11-26 20:06:38 -0300

    Matriz

---
+++ Arrays/TesteMatriz.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class TesteMatriz {
+   public static void main (String[] args) {
+      MatrizQuadrada obj = new MatrizQuadrada();
+      obj.matriz();
+   }
+}
